06c551e13efccba91d2909f8f03b7bb9cfc75a9b
Will Foster <wfoster92@gmail.com>
2021-08-02 14:19:52 -0400

    added global variables reference

---
+++ src/helperFunctions/globals.js
@@ -0,0 +1,10 @@
+// import kelly from "../images/kelly.jpg";
+// import nelly from "../images/nelly.jpg";
+export const squareSize = 80; 
+export const minRows = 3;
+export const maxRows = 3;
+export const minCols = 3;
+export const maxCols = 3;
+export const maxSelections = 3;
+export const colorBase = ["#0000ff", "#ff0000", "#00ff00", "#6442bd"];
+// export const playerImgs = [kelly, nelly];
