c7046aef17a8ad4e440ab8fffd7adf1d80003576
Will Foster <wfoster92@gmail.com>
2021-08-12 19:29:08 -0400

    updated secondsPerRound

--- src/helperFunctions/globals.js
+++ src/helperFunctions/globals.js
@@ -9,7 +9,7 @@ export const colorBase = ["#0000ff", "#ff0000", "#00ff00", "#6442bd",
 export const playerDesigns = ["japanese-cube", "bricks"];
 // export const [difficulty, setDifficulty] = useState(4);
 export const maxNoChangeRounds = 3;
-export const secondsPerRound = 10;
+export const secondsPerRound = 20;
 
 // export function setFormVals(rows, cols, diff) {
 //     numRows = rows;
