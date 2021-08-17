85c6791658f283815ee6123b18abf917addf3a5c
Will Foster <wfoster92@gmail.com>
2021-08-17 18:20:38 -0400

    dumb text

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,7 +4,7 @@ import Footer from "./Footer";
 function Home() {
     return (
         <div>
-        <h1>HOME</h1>
+        <h1>Everything's done except for the homepage :/</h1>
         <div className="footer" style={{marginBottom:0}}>
             <Footer />
         </div>
