2ca0d97a08930d0f14432f156ba9a437bed5416a
Will Foster <wfoster92@gmail.com>
2021-08-27 15:12:29 -0400

    added to home page

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Footer() {
 
     return (
-        <div>
+        <div className="footer" style={{marginTop:5+"vmin"}}>
             Player Icons Made By:
             <a href="https://www.freepik.com" title="Freepik"> <br/>Freepik </a> From <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>
         </div>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,15 +3,12 @@ import Footer from "./Footer";
 
 function Home() {
     return (
-        <div>
-        <h1>Dilemma</h1>
-        {/* <div className="row self-align-items">
-
-        </div> */}
-
-        <div className="footer" style={{marginBottom:0}}>
+        <div className="align-items-center home">
+            <h1> Dilemma</h1>
+            {/* <div className="align-items-center"> */}
+            <img src={`/images/arena.png`} id="homeImg"/>
+            {/* </div> */}
             <Footer />
-        </div>
         </div>
         
     )
