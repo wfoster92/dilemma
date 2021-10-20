48687a5205974f36031466e9a472fff67eceb508
Will Foster <wfoster92@gmail.com>
2021-10-20 11:37:02 -0400

    deleted old code

--- src/index.js
+++ src/index.js
@@ -1,10 +1,3 @@
-// guide to combining bootstrap with react
-// https://blog.logrocket.com/how-to-use-bootstrap-with-react-a354715d1121/
-// import 'bootstrap/dist/css/bootstrap.min.css';
-// import $ from 'jquery';
-// import { createPopper } from '@popperjs/core';
-// import 'bootstrap/dist/js/bootstrap.bundle.min';
-
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
