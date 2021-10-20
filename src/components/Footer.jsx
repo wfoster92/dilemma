11852c892a32aa85411307bbe05ec7efa5f720ff
Will Foster <wfoster92@gmail.com>
2021-10-20 11:36:17 -0400

    Gave warning about dark mode

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,9 +3,11 @@ import React from "react";
 function Footer() {
 
     return (
-        <div className="footer" style={{marginTop:5+"vmin"}}>
-            Player Icons Made By:
-            <a href="https://www.freepik.com" title="Freepik"> <br/>Freepik </a> From <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>
+        <div className="footer">
+            <div>Turn Off Dark Mode For The Best User Experience</div>
+            <div className="disclaimer">
+                Player Icons Made By:<a href="https://www.freepik.com" title="Freepik"> Freepik </a> From <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>
+            </div>
         </div>
     )
 }
